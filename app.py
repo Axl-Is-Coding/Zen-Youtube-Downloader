deda5cb550bde7b35be606b04deba7241ab2d03e
Axl-Is-Coding <dhanaxlmaglinte@gmail.com>
2025-11-17 06:49:59 +0800

    Add Flask app for YouTube video downloads

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, send_file
+from pytube import YouTube
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@app.route("/download", methods=["POST"])
+def download():
+    url = request.form["url"]
+
+    yt = YouTube(url)
+    stream = yt.streams.get_highest_resolution()
+    file_path = stream.download()
+
+    return send_file(file_path, as_attachment=True)
+
+if __name__ == "__main__":
+    app.run(debug=True)
